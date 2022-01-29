6cc9023b6ef5649ce4cf6dcbc44c9fb5bf059fd6
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2022-01-29 12:46:02 -0700

    Add files via upload

--- src/owncat.rkt
+++ src/owncat.rkt
@@ -8,10 +8,10 @@
 ;(string? (vector-ref (current-command-line-arguments) 0))
 
 (define (owncat filename)
-        (system (string-append "echo -n 'Owner: ';" (string-append (string-append "ls -l " filename) "| awk '{print $3}'")))
-        (system (string-append "echo -n 'Group: ';" (string-append (string-append "ls -l " filename) "| awk '{print $4}'")))
-        (system (string-append "echo -n 'Octal: ';" (string-append (string-append "stat -c '%a %n' " filename) "| awk '{print $1}'")))
-        (system (string-append "echo -n 'r/w/x: ';" (string-append (string-append "ls -l " filename) "| awk '{print $1}' | sed 's/#\\.//'"))))
+  (system (string-append "echo -n 'Owner: ';" (string-append (string-append "ls -l " filename) "| awk '{print $3}'")))
+  (system (string-append "echo -n 'Group: ';" (string-append (string-append "ls -l " filename) "| awk '{print $4}'")))
+  (system (string-append "echo -n 'Octal: ';" (string-append (string-append "stat -c '%a %n' " filename) "| awk '{print $1}'")))
+  (system (string-append "echo -n 'r/w/x: ';" (string-append (string-append "ls -l " filename) "| awk '{print $1}' | sed 's/#\\.//'"))))
 
 (run owncat)
 
