89e9190b07512578a534314a683965b0804a5af7
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2022-01-30 10:01:31 -0700

    Update header.rkt

--- src/header.rkt
+++ src/header.rkt
@@ -15,5 +15,7 @@
 (define (run fn)
   (if (= 0 (vector-length (current-command-line-arguments)))
     (exit)
+    ; Loop here
+    ; https://beautifulracket.com/explainer/loops.html
     (system-display (fn (vector-ref (current-command-line-arguments) 0)))))
 
