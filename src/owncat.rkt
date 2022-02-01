e30fa5112ca3099229e58820584e55ce7f02040b
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2022-02-01 14:40:46 -0700

    Update owncat.rkt

--- src/owncat.rkt
+++ src/owncat.rkt
@@ -4,9 +4,6 @@
 (require racket/port)
 (require "header.rkt")
 
-; Returns True:
-;(string? (vector-ref (current-command-line-arguments) 0))
-
 (define (owncat filename)
   (system (string-append "echo -n 'Owner: ';" (string-append (string-append "ls -l " filename) "| awk '{print $3}'")))
   (system (string-append "echo -n 'Group: ';" (string-append (string-append "ls -l " filename) "| awk '{print $4}'")))
@@ -14,4 +11,3 @@
   (system (string-append "echo -n 'r/w/x: ';" (string-append (string-append "ls -l " filename) "| awk '{print $1}' | sed 's/#\\.//'"))))
 
 (run owncat)
-
