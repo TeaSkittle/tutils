35d38b6b662c64234102a3411b8fd1072496c7de
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2022-04-08 18:39:44 +0000

    Update tempr.rkt

--- src/tempr.rkt
+++ src/tempr.rkt
@@ -1,8 +1,7 @@
 #!/usr/bin/racket
 #lang racket/base
-
-
 (require racket/system)
+(require "header.rkt")
 
 ; W.I.P.
 
@@ -10,5 +9,4 @@
 (define (convert c)
   (exact->inexact (+ (* c 9/5) 32)))
 
-(define (get-temps)
-  (system "cat /sys/class/thermal/thermal_zone*/temp"))
+(define get-temps "cat /sys/class/thermal/thermal_zone*/temp")
