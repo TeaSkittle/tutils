1a481917f81d3d7de690026f1adf2bbb8b8d88ec
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2022-04-15 21:14:18 +0000

    Create install.rkt

---
+++ install.rkt
@@ -0,0 +1,11 @@
+#!/usr/bin/racket
+#lang racket/base
+
+; Install script, run via:
+; curl https://raw.githubusercontent.com/TeaSkittle/tutils/main/install.rkt | racket
+
+(newline)
+(displayln "Testing...")
+;(system "git clone https://github.com/TeaSkittle/tutils")
+;(system "cd tutils/src")
+;(system "raco exe -o app app.rkt) ; not accurate, placeholder
