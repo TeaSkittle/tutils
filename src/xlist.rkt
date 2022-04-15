e3e68e6fad888b11ffd8e0048fd1086ad4e05388
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2022-04-15 17:26:48 +0000

    Update xlist.rkt

--- src/xlist.rkt
+++ src/xlist.rkt
@@ -3,7 +3,5 @@
 (require "header.rkt")
 
 (system-display
- (run-subprocess-pipeline '(ls -al)
-                          '(grep -Ei "^[rwx-]{9}x")
-                          '(awk "{print $9}")
-                          '(tail -n +2)))
+ (run-subprocess-pipeline '(ls -al) '(grep -Ei "^[rwx-]{9}x")
+                          '(awk "{print $9}") '(tail -n +2)))
