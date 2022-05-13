35825950066c903653e34931febd0332d3c74a04
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2022-05-13 15:05:15 -0600

    Update scanr.rkt

--- src/scanr.rkt
+++ src/scanr.rkt
@@ -6,8 +6,7 @@
 
 (define (command-list target)
   (string-append (string-append "nc -vzw1 " target)
-                 " 1-162 \
-443 587 993 1735 8008 8080 8443 2>&1 | grep succeeded"))
+                 " 1-162 443 587 993 1735 8008 8080 8443 2>&1 | grep succeeded"))
 
 (define (port-scan target)
   (lead-echo "" (command-list target)))
