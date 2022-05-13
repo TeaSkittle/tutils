02a1efc873c62c830a1b5265b69a580c76bec144
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2022-05-13 15:02:54 -0600

    Add files via upload

---
+++ src/scanr.rkt
@@ -0,0 +1,15 @@
+#!/usr/bin/racket
+#lang racket/base
+(require "header.rkt")
+
+; Scan common ports usin netcat
+
+(define (command-list target)
+  (string-append (string-append "nc -vzw1 " target)
+                 " 1-162 \
+443 587 993 1735 8008 8080 8443 2>&1 | grep succeeded"))
+
+(define (port-scan target)
+  (lead-echo "" (command-list target)))
+
+(run port-scan)
