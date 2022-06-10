10a96be0be3643d93ebdb2e6f475caf61bfc55e3
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2022-06-10 08:24:40 -0600

    Update getsite.rkt

--- src/getsite.rkt
+++ src/getsite.rkt
@@ -5,7 +5,7 @@
 ; Will most likely rename file
 
 (define (get-site domain url)
-  (string-append (string-append "wget --recursive --no-clobber --page-requisites
+  (string-append (string-append "wget --recursive --no-clobber --page-requisites 
 --html-extension --convert-links --restrict-file-names=windows " domain)
                  " --no-parent " url))
 
